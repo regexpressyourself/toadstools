ababd1492d427f6fb886c35188063fd3de61d522
regexpressyourself <sam@bitovi.com>
2020-06-27 09:17:42 -0600

    SM - build

---
+++ front/build/precache-manifest.70f3919573a5d946c2db16e877420857.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6edc5f3c9432dc92d68f760232645d5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "67591b0e10a76fb5ba8d",
+    "url": "/static/css/main.22a92246.chunk.css"
+  },
+  {
+    "revision": "c32cd88c9dff1192ef41",
+    "url": "/static/js/2.625dc828.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.625dc828.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67591b0e10a76fb5ba8d",
+    "url": "/static/js/main.ef1f3f05.chunk.js"
+  },
+  {
+    "revision": "23f7e4d2c8397ce55bf6",
+    "url": "/static/js/runtime-main.5fe610d3.js"
+  },
+  {
+    "revision": "0a27a08fee40a801c760b2b4613d7340",
+    "url": "/static/media/toadstools-simple.0a27a08f.png"
+  },
+  {
+    "revision": "3b6f341626376cdfbc046281b0bf8fc9",
+    "url": "/static/media/toadstools.3b6f3416.png"
+  }
+]);
--- front/build/service-worker.js
+++ front/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8ad2144a99498094782e6fbc076c0373.js"
+  "/precache-manifest.70f3919573a5d946c2db16e877420857.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
